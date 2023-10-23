c8ced86ef76c0a76374cf6a49cb2c800bdcf9c56
Lynxgsm <aili.fida.ac@gmail.com>
2023-10-23 19:43:47 +0200

    first commit: creation base with react

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 import { ButtonProps } from "./Button.props";
 
 export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+  const [open, setopen] = useState(false);
+  const toggle = () => {
+    setopen((_state) => !_state);
+  };
+
+  return <button onClick={toggle}>{open ? "open" : children}</button>;
 }
